7d82909a805b0017a2d880e0482e3e691f54ebf7
dylpckl <dylanjbsmith@gmail.com>
2023-03-28 23:46:52 -0400

    Correctly updating state from the checkboxes

--- src/app/Filter.tsx
+++ src/app/Filter.tsx
@@ -4,29 +4,44 @@ import { videos } from "../app/lib/videos";
 import Video from "./Video";
 
 interface Tag {
-  id: number;
   value: string;
-  checked: boolean;
+  active: boolean;
 }
 
 // TODO - reword tags object, don't need id
 
 const testTags: Tag[] = [
-  { id: 1, value: "improve sleep", checked: false },
-  { id: 2, value: "sleep quality", checked: false },
-  { id: 3, value: "sleep better", checked: false },
-  { id: 4, value: "how to slee better", checked: false },
-  { id: 5, value: "how to improv sleep", checked: false },
-  { id: 6, value: "optimize sleep", checked: false },
-  { id: 7, value: "get better slee", checked: false },
-  { id: 8, value: "circadian clock", checked: false },
-  { id: 9, value: "circadian rhythm", checked: false },
-  { id: 10, value: "stay asleep", checked: false },
-  { id: 11, value: "fall back alsee", checked: false },
-  { id: 12, value: "lifespan", checked: false },
-  { id: 13, value: "procrastination", checked: false },
-  { id: 14, value: "andrew huberman", checked: false },
+  { value: "improve sleep", active: false },
+  { value: "sleep quality", active: false },
+  { value: "sleep better", active: false },
+  { value: "how to slee better", active: false },
+  { value: "how to improv sleep", active: false },
+  { value: "optimize sleep", active: false },
+  { value: "get better slee", active: false },
+  { value: "circadian clock", active: false },
+  { value: "circadian rhythm", active: false },
+  { value: "stay asleep", active: false },
+  { value: "fall back alsee", active: false },
+  { value: "lifespan", active: false },
+  { value: "procrastination", active: false },
+  { value: "andrew huberman", active: false },
 ];
+// const testTags: Tag[] = [
+//   { id: 1, value: "improve sleep", checked: false },
+//   { id: 2, value: "sleep quality", checked: false },
+//   { id: 3, value: "sleep better", checked: false },
+//   { id: 4, value: "how to slee better", checked: false },
+//   { id: 5, value: "how to improv sleep", checked: false },
+//   { id: 6, value: "optimize sleep", checked: false },
+//   { id: 7, value: "get better slee", checked: false },
+//   { id: 8, value: "circadian clock", checked: false },
+//   { id: 9, value: "circadian rhythm", checked: false },
+//   { id: 10, value: "stay asleep", checked: false },
+//   { id: 11, value: "fall back alsee", checked: false },
+//   { id: 12, value: "lifespan", checked: false },
+//   { id: 13, value: "procrastination", checked: false },
+//   { id: 14, value: "andrew huberman", checked: false },
+// ];
 
 let nextId = 0; // initialize a number to increment for the purpose of creating id's
 
@@ -42,20 +57,40 @@ export default function Filter() {
   // const [filteredVideos, setFilteredVideos] = useState<Video[]>([]);
 
   // This function is our even handler that will be sent down the child functions
-  function handleUpdateTags(id: number, value: string, checked: boolean) {
-    // If the input is unchecked, remove the corresponding tag
-    // see: https://react.dev/learn/updating-arrays-in-state#removing-from-an-array
-    if (checked === false) {
-      setTags(tags.filter((t) => t.id !== id));
+  function handleUpdateTags(value: string, active: boolean) {
+    // If the input is unchecked, update active to false
+    if (active === false) {
+      setTags(
+        tags.map((tag) => {
+          if (tag.value === value) {
+            // Create a *new* object with changes
+            return { ...tag, active: false };
+          }
+          // No changes
+          return tag;
+        })
+      );
     } else {
       // otherwise, update state
       // if the tags array doesn't contain an element with id equal to that passed into handleUpdateTags
-      if (tags.some((tag) => tag.id !== id)) {
-        //  1. replace the state with a new array, that contains all of the old items
-        //  2. and a new item: object with properties id and value,
-        //  3. where id is nextId++, value is the value passed into handleUpdateTags, checked is true
-        //  see: https://react.dev/learn/updating-arrays-in-state#adding-to-an-array
-        setTags([...tags, { id: nextId++, value: value, checked: true }]);
+      if (tags.some((tag) => tag.value !== value)) {
+        // console.log(
+        //   tags,
+        //   value,
+        //   tags.some((e) => e.value === value)
+        // );
+        setTags(
+          tags.map((tag) => {
+            if (tag.value === value) {
+              // Create a *new* object with changes
+              return { ...tag, active: true };
+            }
+            // No changes
+            return tag;
+          })
+        );
+
+        // see https://react.dev/learn/updating-arrays-in-state#updating-objects-inside-arrays
       }
     }
   }
@@ -88,18 +123,16 @@ function TagFilter({ tags, onChange }) {
       <h1>filter</h1>
       <ul>
         {tags.map((tag: Tag) => (
-          <div key={tag.id}>
+          <div key={tag.value}>
             <input
               type="checkbox"
               // name={`${tag.value},${tag.id}`}
-              id={tag.id.toString()}
+              id={tag.value}
               // pass the event handler called onChange into the onChange method on the input
               // the syntax to pass an inline function is: onClick={() => alert('...')}
-              onChange={(e) => onChange(tag.id, tag.value, e.target.checked)}
+              onChange={(e) => onChange(tag.value, e.target.checked)}
             />
-            <label
-              htmlFor={tag.id.toString()}
-            >{`${tag.value},${tag.id}`}</label>
+            <label htmlFor={tag.value}>{tag.value}</label>
           </div>
         ))}
       </ul>
