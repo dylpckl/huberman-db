2c1192a05bfb52ade2d8c5b3bd7fe5815948c5de
dylpckl <dylanjbsmith@gmail.com>
2023-03-30 16:25:35 -0400

    add headlessUI, begin SearchBar

--- src/app/Filter.tsx
+++ src/app/Filter.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
+import { Fragment, useState, useEffect } from "react";
 // import { videos } from "../app/lib/videos";
 // import Video from "./Video";
 import VideoClient from "./VideoClient";
+import { UsersIcon } from "@heroicons/react/24/outline";
+import { Combobox, Dialog, Transition } from "@headlessui/react";
 
 interface Tag {
   value: string;
@@ -132,6 +134,8 @@ export default function Filter(videos: Video[]) {
 
   return (
     <>
+      {/* <button onClick={setOpen(!open)}>click</button> */}
+      <SearchBar />
       <TagFilter
         tags={tags}
         onChange={handleUpdateTags}
@@ -179,5 +183,110 @@ function VideoList({ videos }) {
           <VideoClient videoId={video.items[0].id} />
         ))}
     </div>
+  );
+}
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [open, setOpen] = useState(true);
+
+  const people = [
+    { id: 1, name: "Leslie Alexander", url: "#" },
+    // More people...
+  ];
+
+  const filteredPeople =
+    query === ""
+      ? []
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  return (
+    // <h1>searchbar</h1>
+    <Transition.Root
+      show={open}
+      as={Fragment}
+      afterLeave={() => setQuery("")}
+      appear
+    >
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="mx-auto max-w-xl transform rounded-xl bg-white p-2 shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+              <Combobox onChange={(person) => (window.location = person.url)}>
+                <Combobox.Input
+                  className="w-full rounded-md border-0 bg-gray-100 px-4 py-2.5 text-gray-900 focus:ring-0 sm:text-sm"
+                  placeholder="Search..."
+                  onChange={(event) => setQuery(event.target.value)}
+                />
+
+                {filteredPeople.length > 0 && (
+                  <Combobox.Options
+                    static
+                    className="-mb-2 max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
+                  >
+                    {filteredPeople.map((person) => (
+                      <Combobox.Option
+                        key={person.id}
+                        value={person}
+                        className={({ active }) =>
+                          classNames(
+                            "cursor-default select-none rounded-md px-4 py-2",
+                            active && "bg-indigo-600 text-white"
+                          )
+                        }
+                      >
+                        {person.name}
+                      </Combobox.Option>
+                    ))}
+                  </Combobox.Options>
+                )}
+
+                {query !== "" && filteredPeople.length === 0 && (
+                  <div className="px-4 py-14 text-center sm:px-14">
+                    <UsersIcon
+                      className="mx-auto h-6 w-6 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <p className="mt-4 text-sm text-gray-900">
+                      No people found using that search term.
+                    </p>
+                  </div>
+                )}
+              </Combobox>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
